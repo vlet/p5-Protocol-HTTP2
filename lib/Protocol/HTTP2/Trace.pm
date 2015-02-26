8e727ea2ef174bc35b6f2662aaabbc418f5fb093
Vladimir Lettiev <thecrux@gmail.com>
2015-02-26 23:28:51 +0300

    rewrite Trace module without Log::Dispatch

--- lib/Protocol/HTTP2/Trace.pm
+++ lib/Protocol/HTTP2/Trace.pm
@@ -1,19 +1,68 @@
 package Protocol::HTTP2::Trace;
 use strict;
 use warnings;
-use Protocol::HTTP2::Constants qw(:flags :errors);
-use Log::Dispatch;
+use Time::HiRes qw(time);
 
 use Exporter qw(import);
 our @EXPORT_OK = qw(tracer bin2hex);
 
-my $level = ( exists $ENV{HTTP2_DEBUG} ) ? $ENV{HTTP2_DEBUG} : 'critical';
+my %levels = (
+    debug     => 0,
+    info      => 1,
+    notice    => 2,
+    warning   => 3,
+    error     => 4,
+    critical  => 5,
+    alert     => 6,
+    emergency => 7,
+);
 
-my $tracer_sngl =
-  Log::Dispatch->new( outputs => [ [ 'Screen', min_level => $level ], ], );
+my $tracer_sngl = Protocol::HTTP2::Trace->_new(
+    min_level =>
+      ( exists $ENV{HTTP2_DEBUG} && exists $levels{ $ENV{HTTP2_DEBUG} } )
+    ? $levels{ $ENV{HTTP2_DEBUG} }
+    : $levels{error}
+);
+my $start_time = 0;
 
 sub tracer {
     $tracer_sngl;
+}
+
+sub _new {
+    my ( $class, %opts ) = @_;
+    bless {%opts}, $class;
+}
+
+sub _log {
+    my ( $self, $level, $message ) = @_;
+    $level = uc($level);
+    chomp($message);
+    my $now = time;
+    if ( $now - $start_time < 60 ) {
+        $message =~ s/\n/\n           /g;
+        printf "[%05.3f] %s %s\n", $now - $start_time, $level, $message;
+    }
+    else {
+        my @t = ( localtime() )[ 5, 4, 3, 2, 1, 0 ];
+        $t[0] += 1900;
+        $t[1]++;
+        $message =~ s/\n/\n                      /g;
+        printf "[%4d-%02d-%02d %02d:%02d:%02d] %s %s\n", @t, $level, $message;
+        $start_time = $now;
+    }
+}
+
+{
+    no strict 'refs';
+    for my $l ( keys %levels ) {
+        *{ __PACKAGE__ . "::" . $l } =
+          ( $levels{$l} >= $tracer_sngl->{min_level} )
+          ? sub {
+            shift->_log( $l, @_ );
+          }
+          : sub { 1 }
+    }
 }
 
 sub bin2hex {
@@ -26,7 +75,6 @@ sub bin2hex {
         $s = !( $c % 16 ) ? "\n" : ( $c % 2 ) ? "" : " ";
         $_ . $s
     } unpack( "(H2)*", $bin );
-
 }
 
 1;
