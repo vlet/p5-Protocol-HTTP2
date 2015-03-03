fc3b9b793e82305caf36825e86efb1b56a281f7f
Vladimir Lettiev <thecrux@gmail.com>
2015-03-03 10:11:33 +0300

    update pod: status & wiki

--- lib/Protocol/HTTP2.pm
+++ lib/Protocol/HTTP2.pm
@@ -61,9 +61,9 @@ plain or tls socket (see examples).
 
 =head1 STATUS
 
-Current status - alpha. Structures, module names and methods may change vastly.
+Current status - beta. Structures, module names and methods seems like stable.
 I've started this project to understand internals of HTTP/2 and may be it will
-never become production or even finished.
+never become production, but at least it works.
 
     | Spec                    |      status     |
     | ----------------------- | --------------- |
@@ -126,13 +126,19 @@ L<http://tools.ietf.org/html/draft-ietf-httpbis-header-compression-12>
 
 =head2 L<Protocol::HTTP2::Constants>
 
-Module contain all defined in HTTP/2 protocol constants and default values
+Module contains all defined in HTTP/2 protocol constants and default values
 
 =head2 L<Protocol::HTTP2::Trace>
 
-Module for debugging. (Ab)used Log::Dispatch internally, may be removed soon.
+Module for debugging. You can setup HTTP2_DEBUG environment variable to change
+verbosity of the module (output to STDOUT). Default level is error.
+
+    $ export HTTP2_DEBUG=debug
+    $ perl ./http2_program
 
 =head1 SEE ALSO
+
+L<https://github.com/vlet/p5-Protocol-HTTP2/wiki> - Protocol::HTTP2 wiki
 
 L<http://http2.github.io/> - official HTTP/2 specification site
 
